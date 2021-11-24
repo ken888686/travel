9fc464c0e3fb573b25c6c921f1e9e3d8c689ac21
Aaron <ken888686@gmail.com>
2021-11-24 10:56:37 +0800

    add bs5

--- src/main.js
+++ src/main.js
@@ -2,4 +2,8 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
+// Bootstrap 5
+import '@/assets/scss/all.scss';
+import 'bootstrap';
+
 createApp(App).use(router).mount('#app');
