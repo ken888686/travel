070518f4017793ea2dbd287afd5799b3720ba17d
Aaron <ken888686@gmail.com>
2021-11-26 13:47:21 +0800

    add card list

---
+++ src/components/CardList.vue
@@ -0,0 +1,184 @@
+<template>
+  <div class="w-100">
+    <div class="container d-flex justify-content-between">
+      <h3 class="fw-bold">{{ title }}</h3>
+      <button type="button" class="btn btn-primary">查看更多</button>
+    </div>
+    <swiper
+      :slides-per-view="4"
+      :space-between="10"
+      @swiper="onSwiper"
+      @slideChange="onSlideChange"
+    >
+      <swiper-slide>
+        <Card
+          title="新北歡樂耶誕城"
+          content="每年一到12月，歡樂耶誕城都會舉辦盛大的耶誕活動與美麗的裝置藝術，讓整個板橋也有濃濃的耶誕過節氣氛！"
+          :tags="[
+            { id: 0, title: '文化活動' },
+            { id: 1, title: '熱鬧' },
+            { id: 2, title: '一年一度' },
+          ]"
+        />
+      </swiper-slide>
+      <swiper-slide>
+        <Card
+          title="新北歡樂耶誕城"
+          content="每年一到12月，歡樂耶誕城都會舉辦盛大的耶誕活動與美麗的裝置藝術，讓整個板橋也有濃濃的耶誕過節氣氛！"
+          :tags="[
+            { id: 0, title: '文化活動' },
+            { id: 1, title: '熱鬧' },
+            { id: 2, title: '一年一度' },
+          ]"
+        />
+      </swiper-slide>
+      <swiper-slide>
+        <Card
+          title="新北歡樂耶誕城"
+          content="每年一到12月，歡樂耶誕城都會舉辦盛大的耶誕活動與美麗的裝置藝術，讓整個板橋也有濃濃的耶誕過節氣氛！"
+          :tags="[
+            { id: 0, title: '文化活動' },
+            { id: 1, title: '熱鬧' },
+            { id: 2, title: '一年一度' },
+          ]"
+        />
+      </swiper-slide>
+      <swiper-slide>
+        <Card
+          title="新北歡樂耶誕城"
+          content="每年一到12月，歡樂耶誕城都會舉辦盛大的耶誕活動與美麗的裝置藝術，讓整個板橋也有濃濃的耶誕過節氣氛！"
+          :tags="[
+            { id: 0, title: '文化活動' },
+            { id: 1, title: '熱鬧' },
+            { id: 2, title: '一年一度' },
+          ]"
+        />
+      </swiper-slide>
+      <swiper-slide>
+        <Card
+          title="新北歡樂耶誕城"
+          content="每年一到12月，歡樂耶誕城都會舉辦盛大的耶誕活動與美麗的裝置藝術，讓整個板橋也有濃濃的耶誕過節氣氛！"
+          :tags="[
+            { id: 0, title: '文化活動' },
+            { id: 1, title: '熱鬧' },
+            { id: 2, title: '一年一度' },
+          ]"
+        />
+      </swiper-slide>
+      <swiper-slide>
+        <Card
+          title="新北歡樂耶誕城"
+          content="每年一到12月，歡樂耶誕城都會舉辦盛大的耶誕活動與美麗的裝置藝術，讓整個板橋也有濃濃的耶誕過節氣氛！"
+          :tags="[
+            { id: 0, title: '文化活動' },
+            { id: 1, title: '熱鬧' },
+            { id: 2, title: '一年一度' },
+          ]"
+        />
+      </swiper-slide>
+      <swiper-slide>
+        <Card
+          title="新北歡樂耶誕城"
+          content="每年一到12月，歡樂耶誕城都會舉辦盛大的耶誕活動與美麗的裝置藝術，讓整個板橋也有濃濃的耶誕過節氣氛！"
+          :tags="[
+            { id: 0, title: '文化活動' },
+            { id: 1, title: '熱鬧' },
+            { id: 2, title: '一年一度' },
+          ]"
+        />
+      </swiper-slide>
+      <swiper-slide>
+        <Card
+          title="新北歡樂耶誕城"
+          content="每年一到12月，歡樂耶誕城都會舉辦盛大的耶誕活動與美麗的裝置藝術，讓整個板橋也有濃濃的耶誕過節氣氛！"
+          :tags="[
+            { id: 0, title: '文化活動' },
+            { id: 1, title: '熱鬧' },
+            { id: 2, title: '一年一度' },
+          ]"
+        />
+      </swiper-slide>
+      <swiper-slide>
+        <Card
+          title="新北歡樂耶誕城"
+          content="每年一到12月，歡樂耶誕城都會舉辦盛大的耶誕活動與美麗的裝置藝術，讓整個板橋也有濃濃的耶誕過節氣氛！"
+          :tags="[
+            { id: 0, title: '文化活動' },
+            { id: 1, title: '熱鬧' },
+            { id: 2, title: '一年一度' },
+          ]"
+        />
+      </swiper-slide>
+      <swiper-slide>
+        <Card
+          title="新北歡樂耶誕城"
+          content="每年一到12月，歡樂耶誕城都會舉辦盛大的耶誕活動與美麗的裝置藝術，讓整個板橋也有濃濃的耶誕過節氣氛！"
+          :tags="[
+            { id: 0, title: '文化活動' },
+            { id: 1, title: '熱鬧' },
+            { id: 2, title: '一年一度' },
+          ]"
+        />
+      </swiper-slide>
+      <swiper-slide>
+        <Card
+          title="新北歡樂耶誕城"
+          content="每年一到12月，歡樂耶誕城都會舉辦盛大的耶誕活動與美麗的裝置藝術，讓整個板橋也有濃濃的耶誕過節氣氛！"
+          :tags="[
+            { id: 0, title: '文化活動' },
+            { id: 1, title: '熱鬧' },
+            { id: 2, title: '一年一度' },
+          ]"
+        />
+      </swiper-slide>
+      <swiper-slide>
+        <Card
+          title="新北歡樂耶誕城"
+          content="每年一到12月，歡樂耶誕城都會舉辦盛大的耶誕活動與美麗的裝置藝術，讓整個板橋也有濃濃的耶誕過節氣氛！"
+          :tags="[
+            { id: 0, title: '文化活動' },
+            { id: 1, title: '熱鬧' },
+            { id: 2, title: '一年一度' },
+          ]"
+        />
+      </swiper-slide>
+      <swiper-slide>
+        <Card
+          title="新北歡樂耶誕城"
+          content="每年一到12月，歡樂耶誕城都會舉辦盛大的耶誕活動與美麗的裝置藝術，讓整個板橋也有濃濃的耶誕過節氣氛！"
+          :tags="[
+            { id: 0, title: '文化活動' },
+            { id: 1, title: '熱鬧' },
+            { id: 2, title: '一年一度' },
+          ]"
+        />
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+<script>
+import { Swiper, SwiperSlide } from 'swiper/vue/swiper-vue';
+import 'swiper/swiper.scss';
+
+import Card from './Card.vue';
+
+export default {
+  data() {
+    return { title: '熱門打卡景點' };
+  },
+  components: {
+    Card,
+    Swiper,
+    SwiperSlide,
+  },
+  methods: {
+    onSwiper(swiper) {
+      console.log(swiper);
+    },
+    onSlideChange() {
+      console.log('slide change');
+    },
+  },
+};
+</script>
+<style lang="scss" scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,14 @@
 <template>
   <Header />
   <Banner />
-  <Card
-    title="新北歡樂耶誕城"
-    content="每年一到12月，歡樂耶誕城都會舉辦盛大的耶誕活動與美麗的裝置藝術，讓整個板橋也有濃濃的耶誕過節氣氛！"
-    :tags="[
-      { id: 0, title: '文化活動' },
-      { id: 1, title: '熱鬧' },
-      { id: 2, title: '一年一度' },
-    ]"
-  />
+  <CardList />
   <Footer />
 </template>
 
 <script>
 import Header from '@/components/Header.vue';
 import Banner from '@/components/Banner.vue';
-import Card from '@/components/Card.vue';
+import CardList from '@/components/CardList.vue';
 import Footer from '@/components/Footer.vue';
 
 export default {
@@ -26,7 +18,7 @@ export default {
   components: {
     Header,
     Banner,
-    Card,
+    CardList,
     Footer,
   },
 };
