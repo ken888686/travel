85fe35840ff0fce71beafb17c44c8d5ce442c023
ken888686@gmail.com <asas456789>
2021-12-06 23:47:03 +0800

    custom button

--- src/main.js
+++ src/main.js
@@ -2,4 +2,7 @@ import { createApp } from 'vue';
 import App from '@/App.vue';
 import router from '@/router';
 
+import '@/assets/scss/all.scss';
+import 'bootstrap';
+
 createApp(App).use(router).mount('#app');
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,12 @@
 <template>
-  <div class="home">
+  <!-- <div class="home">
     <img alt="Vue logo" src="@/assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  </div> -->
+  <button type="button" class="btn btn-primary">找景點</button>
+  <button type="button" class="btn tag tag-outline" disabled>文化活動</button>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
-};
+<script setup>
+// import HelloWorld from '@/components/HelloWorld.vue';
 </script>
