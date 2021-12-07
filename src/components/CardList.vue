bd1c77773796f25b869b201999bd816a90c9ccc6
ken888686@gmail.com <asas456789>
2021-12-07 23:43:02 +0800

    add card

---
+++ src/components/CardList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container-fluid mt-4">
+    <div class="container d-flex justify-content-between align-items-center">
+      <h3>{{ title }}</h3>
+      <button type="button" class="btn btn-primary">查看更多</button>
+    </div>
+    <div class="row">
+      <div class="col">
+        <div class="card">
+          <img src="@/assets/images/card-img.png" class="card-img-top" alt="Card" />
+          <div class="card-body">
+            <h5 class="card-title">新北市歡樂耶誕城</h5>
+            <button class="tag">文化活動</button>
+            <button class="tag">熱鬧</button>
+            <button class="tag">新北板橋</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+const title = '熱門打卡景點';
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <Banner />
+  <CardList />
 </template>
 
 <script setup>
 import Banner from '@/components/Banner.vue';
+import CardList from '@/components/CardList.vue';
 </script>
