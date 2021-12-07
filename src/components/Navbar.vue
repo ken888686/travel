9975efd54f1051d8034abf6a61c621a0bcbc022c
ken888686@gmail.com <asas456789>
2021-12-07 21:18:46 +0800

    add Banner

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -22,26 +22,26 @@
             <img src="@/assets/icons/search.svg" alt="search" style="height: 14px; width: 14px" />
           </button>
         </form>
-        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <router-link to="/tour" class="btn btn-outline-primary">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0 row">
+          <li class="nav-item col">
+            <router-link to="/tour" class="btn btn-outline-primary text-nowrap">
               找景點
               <img src="@/assets/icons/tour.svg" alt="找景點" />
             </router-link>
           </li>
-          <li class="nav-item">
-            <router-link to="/food" class="btn btn-outline-primary">
+          <li class="nav-item col">
+            <router-link to="/food" class="btn btn-outline-primary text-nowrap">
               找餐廳
               <img src="@/assets/icons/food.svg" alt="找餐廳" />
             </router-link>
           </li>
-          <li class="nav-item">
-            <router-link to="/hotel" class="btn btn-outline-primary">
+          <li class="nav-item col">
+            <router-link to="/hotel" class="btn btn-outline-primary text-nowrap">
               找飯店
               <img src="@/assets/icons/bed.svg" alt="找飯店" />
             </router-link>
           </li>
-          <li class="nav-item">
+          <li class="nav-item col">
             <router-link to="/my-journey" class="btn btn-outline-primary">我的旅程</router-link>
           </li>
         </ul>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,7 @@
 <template>
-  <!-- <div class="home">
-    <img alt="Vue logo" src="@/assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div> -->
-  <div class="container text-center">
-    <h1>{{ title }}</h1>
-  </div>
+  <Banner />
 </template>
 
 <script setup>
-// import HelloWorld from '@/components/HelloWorld.vue';
-const title = 'Home';
+import Banner from '@/components/Banner.vue';
 </script>
