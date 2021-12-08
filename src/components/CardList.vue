45aaf230e1fd354fe9cf28b48a025a64b497717c
Aaron <ken888686@gmail.com>
2021-12-08 18:45:33 +0800

    add like button

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container-fluid mt-4">
+  <div class="container-fluid my-4">
     <div class="container d-flex justify-content-between align-items-center">
       <h3>{{ title }}</h3>
       <button type="button" class="btn btn-primary">查看更多</button>
@@ -7,7 +7,12 @@
     <div class="row">
       <div class="col">
         <div class="card">
-          <img src="@/assets/images/card-img.png" class="card-img-top" alt="Card" />
+          <div class="">
+            <img src="@/assets/images/card-img.png" class="card-img-top" alt="Card" />
+            <button type="button" class="btn btn-outline-primary btn-like">
+              <img src="@/assets/icons/like.svg" alt="like" />
+            </button>
+          </div>
           <div class="card-body">
             <h5 class="card-title">新北市歡樂耶誕城</h5>
             <button class="tag">文化活動</button>
@@ -22,3 +27,4 @@
 <script setup>
 const title = '熱門打卡景點';
 </script>
+<style lang="scss"></style>
