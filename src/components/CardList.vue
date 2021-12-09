d3e60fe07060d5a9ef528b5910fc064a72fd27a1
ken888686@gmail.com <asas456789>
2021-12-09 22:43:19 +0800

    add card swiper

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="container-fluid my-4">
-    <div class="container d-flex justify-content-between align-items-center">
-      <h3>熱門打卡景點</h3>
+    <div class="container d-flex justify-content-between align-items-center my-3">
+      <h3 class="m-0">
+        熱門打卡景點
+      </h3>
       <button
         type="button"
         class="btn btn-primary"
@@ -9,29 +11,39 @@
         查看更多
       </button>
     </div>
-    <div class="row">
-      <div
+    <swiper
+      :slidesPerView="3"
+      :freeMode="true"
+    >
+      <swiper-slide
         v-for="(item, index) in cardInfo"
         :key="index"
-        class="col"
       >
         <Card
-          :title="item.title"
+          :title="`${item.title} ${index + 1}`"
           :tags="item.tags"
         />
-      </div>
-    </div>
+      </swiper-slide>
+    </swiper>
   </div>
 </template>
 <script setup>
+import { Swiper, SwiperSlide } from 'swiper/vue/swiper-vue';
+import SwiperCore, { FreeMode } from 'swiper/swiper-bundle.esm';
 import Card from '@/components/Card.vue';
+import 'swiper/swiper.min.css';
+import 'swiper/modules/free-mode/free-mode.scss';
 
-const cardInfo = [
-  {
-    title: '新北歡樂耶誕城',
-    score: 3,
-    tags: ['文化活動', '熱鬧', '新北板橋'],
-  },
-];
+SwiperCore.use([FreeMode]);
+
+const card = {
+  title: '新北歡樂耶誕城',
+  score: 4.6,
+  tags: ['文化活動', '熱鬧', '新北板橋'],
+};
+const cardInfo = [];
+for (let i = 0; i < 10; i += 1) {
+  cardInfo.push(card);
+}
 </script>
 <style lang="scss"></style>
